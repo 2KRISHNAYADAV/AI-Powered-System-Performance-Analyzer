198c6f0947ece15420e0e7ef92ad21b47a25c891
krishna yadav <143177609+2KRISHNAYADAV@users.noreply.github.com>
2026-06-16 01:50:07 +0530

    Delete newholisystemc++ directory

--- newholisystemc++/plugins/example_plugin.py
+++
@@ -1,9 +0,0 @@
-"""
-Example Plugin for SystemHero
-To use, ensure the main dashboard passes itself to `setup`.
-"""
-from PyQt6.QtWidgets import QMessageBox
-
-def setup(main_window):
-    print("Example Plugin initialized!")
-    return "ExamplePlugin"
